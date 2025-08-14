ff3f67e5b1d4d27691baab211b6205ebe9cdf9e7
gangasagar12 <ganga15bca2024@kcc.edu.np>
2025-08-14 12:27:52 +0545

    Updated content

--- Base_App/views.py
+++ Base_App/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def HomeView(request):
+    return render(request,'home.html')
+def AboutView(request):
+    pass
+def MenuView(request):
+    pass
+
+def BookTableView(request):
+    pass
